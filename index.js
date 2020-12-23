961dcf7d3af65908d7620fa1019f6b2da6bc7ad5
Ligrade <ligrade13@gmail.com>
2020-12-24 00:17:24 +0100

    add ping

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //--------------dépendance--------------
 const Discord = require("discord.js");
 const config = require("./config.json");
+const prefix = ("!");
 //--------------------------------------
 
 
@@ -14,7 +15,23 @@ const client = new Discord.Client();
     });
     //--------------------------------
 
-
+    client.on('message', msg => {
+        if(msg.content.startsWith(`${prefix}ping`)) {
+      
+        let embed = new Discord.MessageEmbed()
+              .setColor(0x43f033)
+              .setDescription(`Loading...`)
+              .setTimestamp()
+          msg.channel.send(embed).then(msg => {
+              embed.setColor(0x43f033)
+              embed.setDescription(`:ping_pong: Pong ! **${Math.round(client.ws.ping)}ms**`)
+              embed.setFooter(`!ping`)
+              embed.setTimestamp()
+              msg.edit(embed)
+          })
+        
+        }
+    });
 
 //------------Token------------
 client.login(config.BOT_TOKEN);
