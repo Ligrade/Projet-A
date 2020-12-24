628cb2a3bd6debd214a4c93009a39c00a4c1cfe3
BuildTools <unconfigured@null.spigotmc.org>
2020-12-24 17:20:28 +0100

    Ajout d'alexa troll

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 //--------------dépendance--------------
 const Discord = require("discord.js");
 const config = require("./config.json");
+let alexa = require('alexa-bot-api');
 const prefix = ("!");
 //--------------------------------------
+
+
+//--------------API---------------
+let chatbot = new alexa("aw2plm")
+//--------------------------------
 
 
 //--------------def client-------------
 const client = new Discord.Client();
 //-------------------------------------
+
 
     //----------Power Message---------
     client.on("ready", () => {
@@ -15,23 +22,41 @@ const client = new Discord.Client();
     });
     //--------------------------------
 
+
+    //-----------------------------------------Actions-----------------------------------------------------
     client.on('message', msg => {
+
+
+        //------------------------!Alexa-----------------------------
+        if(msg.content.startsWith(`${prefix}alexa`)){
+            if(msg.author.bot) return;
+                let cont = msg.content;
+                chatbot.getReply(cont).then(r => msg.channel.send(r))
+        }
+        //------------------------------------------------------------
+
+
+        //--------------------------------------!Ping-----------------------------------------
         if(msg.content.startsWith(`${prefix}ping`)) {
       
-        let embed = new Discord.MessageEmbed()
-              .setColor(0x43f033)
-              .setDescription(`Loading...`)
-              .setTimestamp()
-          msg.channel.send(embed).then(msg => {
-              embed.setColor(0x43f033)
-              embed.setDescription(`:ping_pong: Pong ! **${Math.round(client.ws.ping)}ms**`)
-              embed.setFooter(`!ping`)
-              embed.setTimestamp()
-              msg.edit(embed)
+            let embed = new Discord.MessageEmbed()
+                .setColor(0x43f033)
+                .setDescription(`Loading...`)
+                .setTimestamp()
+            msg.channel.send(embed).then(msg => {
+                embed.setColor(0x43f033)
+                embed.setDescription(`:ping_pong: Pong ! **${Math.round(client.ws.ping)}ms**`)
+                embed.setFooter(`!ping`)
+                embed.setTimestamp()
+                msg.edit(embed)
           })
         
         }
+        //--------------------------------------------------------------------------------------
+
     });
+    //-----------------------------------------------------------------------------------------------------
+
 
 //------------Token------------
 client.login(config.BOT_TOKEN);
